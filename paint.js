6a8b311e0c08c20d714d1ede438030eda6576eeb
Gry4hon <77369398+Gry4hon@users.noreply.github.com>
2023-12-12 16:01:42 -0800

    Beginning To Implement Fill Tool
    Just need to study the flood fill algorithm, and then we'll be golden!

--- paint.js
+++ paint.js
@@ -5,7 +5,7 @@ const paintCanvasHeight = paintCanvas.clientHeight;
 
 const pennTool = document.getElementById("penn");
 const eraserTool = document.getElementById("eraser");
-const fillTool = document.getElementById("fill-tool");
+const fillTool = document.getElementById("fill-bucket");
 const colorPicker = document.getElementById("color-picker");
 const sizePicker = document.getElementById("size-picker");
 
@@ -14,15 +14,34 @@ paintCanvas.addEventListener("mousedown", BeginDrawing);
 paintCanvas.addEventListener("mouseup", StopDrawing);
 document.addEventListener("keydown", SwitchTool);
 
+
+
+pennTool.onclick = ()=>{
+    currentTool = 1;
+}
+
+eraserTool.onclick = ()=>{
+    currentTool = 2;
+}
+
+fillTool.onclick = ()=>{
+    currentTool = 3;
+    SelectFillTool();
+}
+
 let currentTool = 1;
 
+let fillX; 
+let fillY;
+let gettingData = true;
+
+let previousColor;
 
 
 function BeginDrawing(mousedown){
     if(mousedown){
         paintCanvas.addEventListener("mousemove", MovePaintBrush);
     }
-    
 }
 
 function StopDrawing(mouseup){
@@ -36,8 +55,13 @@ function SwitchTool(key){
     if(key.key == '1'){
         currentTool = 1;
     }
-    if(key.key == '2'){
+    else if(key.key == '2'){
         currentTool = 2;
+    }
+    else if(key.key == '3'){
+        currentTool = 3;
+
+        SelectFillTool();
     }
     
 }
@@ -45,7 +69,7 @@ function SwitchTool(key){
 function MovePaintBrush(event){
     const paintCanvasRect = paintCanvas.getBoundingClientRect();
 
-    let courserX = event.x - paintCanvasRect.left;
+    let courserX  = event.x - paintCanvasRect.left;
     let courserY = event.y - paintCanvasRect.top;
 
     switch(currentTool){
@@ -62,5 +86,46 @@ function MovePaintBrush(event){
     
 
     
+}
+
+function SelectFillTool(){
+    
+    paintCanvas.addEventListener("mousedown", (event) =>{
+        if(gettingData){
+            const paintCanvasRect = paintCanvas.getBoundingClientRect();
+            fillX = event.x - paintCanvasRect.left;
+            fillY = event.y - paintCanvasRect.top;
+            FloodFillAlgorithm();
+            gettingData = false;
+        }
+            
+    });
+    paintCanvas.addEventListener("mouseup", () =>{
+        gettingData = true;
+    });
+}
+
+function FloodFillAlgorithm(){
+    if(currentTool == 3){
+
+    const colorData = paintCanvasContext.getImageData(fillX, fillY, 1, 1);
+
+    
+
+    const red = colorData.data[0];
+    const green = colorData.data[1];
+    const blue = colorData.data[2];
+
+    console.log("R:" + red);
+    console.log("G:" + green);
+    console.log("B:" + blue);
+
+    let newColor = colorPicker.value;
+    previousColor = `rgb(${red}, ${green}, ${blue}`;
+
+    
+}
+    
+
 }
 
---
+++ understandingFloodFill.js
@@ -0,0 +1,18 @@
+let screen =[
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0],
+    [1, 0, 0, 1, 1, 0, 1, 1],
+    [1, 2, 2, 2, 2, 0, 1, 0],
+    [1, 1, 1, 2, 2, 0, 1, 0],
+    [1, 1, 1, 2, 2, 2, 2, 0],
+    [1, 1, 1, 1, 1, 2, 1, 1],
+    [1, 1, 1, 1, 1, 2, 2, 1]];
+ 
+    // Row of the display
+    let m = screen.length;
+ 
+    // Column of the display
+    let n = screen[0].length;
+
+    console.log(m);
+    console.log(n);
